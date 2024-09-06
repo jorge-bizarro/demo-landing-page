d7dc06c88251bb14482e23f531a0b4f4a588f4fc
Jorge Bizarro <jorgeluisbizarroreyes@gmail.com>
2024-09-05 19:43:42 -0500

    chore: add navbar template

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,6 +1,7 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
@@ -11,7 +12,8 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    NavbarComponent
   ]
 })
 export class HomeModule { }
